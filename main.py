d043c4f35f63445dbd94a9eb0522817ebaf6de95
Gibran-101 <83395901+Gibran-101@users.noreply.github.com>
2023-12-21 22:56:21 -0500

    Create main.py
    This is the main file of my project.

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+import hangman_art
+import hangman_words
+
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+print(hangman_art.logo)
+
+# print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+      print(f"The letter {guess} has already been entered!")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        print(f"The letter {guess} is not in the word.")
+        lives -= 1
+        print(hangman_art.stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    else:
+      print("Please enter a letter")
+
